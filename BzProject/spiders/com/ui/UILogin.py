54da1e693d74fa5d0166d6978a1b08fcba1d0188
tcm209 <625201089@qq.com>
2019-01-03 13:30:51 +0800

    爬虫工具登录页面

--- BzProject/spiders/com/ui/UILogin.py
+++ BzProject/spiders/com/ui/UILogin.py
@@ -2,7 +2,14 @@
 import tkinter as tk
 import tkinter.messagebox
 from BzProject.spiders.com.bz.DbHelpers import DbHelpers
-from BzProject.spiders.com.StartSpider import StartSpider
+from BzProject.spiders.com.ui.ShowUI import ShowUI
+from PIL import Image,ImageTk
+from BzProject.spiders.com.bz.ImgUtils import ImgUtils
+import pygame
+import random
+import os
+import sys
+import time
 
 
 # 登录UI页面
@@ -15,35 +22,50 @@ class UILogin(object):
         # 设置窗口名称
         self.wd.title("welcome")
         # 设置窗口大小
-        self.wd.geometry("400x300")
+        self.wd.geometry("360x300")
+        self.logo_w = 500
+        self.log_h = 200
+        self.imgTool = ImgUtils()
+        self.fsize=14
+        self.fisty=210
+        self.secondy=250
+        self.wdth=17
+
         # 加载图片
-        canvas = tk.Canvas(self.wd, width=400, height=135, bg="green")
-        image_file = tk.PhotoImage(file="E:\\Pythonwork\\source\\out\\dxout.png")
-        image = canvas.create_image(200, 0, anchor='n', image=image_file)
-        canvas.pack(side="top")
-        tk.Label(self.wd, text="welcome", font=("Arial", 16)).pack()
+        # canvas = tk.Canvas(self.wd, width=400, height=135, bg="green")
+        # image_file = tk.PhotoImage(file="E:\\Pythonwork\\source\\out\\dyht.png")
+        # image = canvas.create_image(200, 0, anchor='n', image=image_file)
+        # canvas.pack(side="top")
+        log_img = Image.open(r'E:\\Pythonwork\\source\\out\\dyht.png')
+        w, h = log_img.size
+        img_resized = self.imgTool.resize(w, h, self.logo_w, self.log_h, log_img)
+        tk_img = ImageTk.PhotoImage(img_resized)
+
+        tk.Label(self.wd, image=tk_img,compound='center').pack()
+        tk.Button(self.wd, text="play",command=self.playBgMusic).place(x=50, y=50)
 
         # 用户信息
-        tk.Label(self.wd, text="User name：", font=("Arial", 14)).place(x=10, y=170)
-        tk.Label(self.wd, text="Password：", font=("Arial", 14)).place(x=10, y=210)
+        tk.Label(self.wd, text="名称", font=("Arial", self.fsize)).place(x=10, y=self.fisty)
+        tk.Label(self.wd, text="密码", font=("Arial", self.fsize)).place(x=10, y=self.secondy)
 
         # 用户登录输入框
         # 用户名
         self.var_user_name = tk.StringVar()
         self.var_user_name.set("wuyl")
-        entry_user_name = tk.Entry(self.wd, textvariable=self.var_user_name, font=("Arial", 14))
-        entry_user_name.place(x=120, y=175)
+        entry_user_name = tk.Entry(self.wd, textvariable=self.var_user_name,relief="sunken", font=("Arial", self.fsize),width=self.wdth)
+        entry_user_name.place(x=80, y=self.fisty)
         # 密码
         self.var_user_pwd = tk.StringVar()
-        entry_user_pwd = tk.Entry(self.wd, textvariable=self.var_user_pwd, font=("Arial", 14), show="*")
-        entry_user_pwd.place(x=120, y=215)
+        entry_user_pwd = tk.Entry(self.wd, textvariable=self.var_user_pwd,relief="sunken", font=("Arial", self.fsize), show="*",width=self.wdth)
+        entry_user_pwd.place(x=80, y=self.secondy)
 
         # login and sign up
-        btn_login = tk.Button(self.wd, text="login", command=self.user_login)
-        btn_login.place(x=120, y=240)
+        btn_login = tk.Button(self.wd, text="登录", command=self.user_login,width=7)
+        btn_login.place(x=290, y=self.fisty)
 
-        btn_sign_up = tk.Button(self.wd, text="sign up", command=self.user_sign_up)
-        btn_sign_up.place(x=200, y=240)
+        btn_sign_up = tk.Button(self.wd, text="注册", command=self.user_sign_up,width=7)
+        btn_sign_up.place(x=290, y=self.secondy)
+
 
         self.wd.mainloop()
 
@@ -58,8 +80,10 @@ class UILogin(object):
             querypwd=row[0]
             if user_pwd == querypwd:
                 tkinter.messagebox.showinfo(title="Welcome", message="how are you"+user_name)
-                startSpider=StartSpider()
-                startSpider.doCmd()
+                # startSpider=StartSpider()
+                sUI=ShowUI()
+                sUI.onshowUI({"username":"wuyl"})
+
             else:
                 tkinter.messagebox.showerror(title="Error",message="密码错误")
         else:
@@ -112,6 +136,20 @@ class UILogin(object):
         #websit位置
         btn_confirm_sign_up=tk.Button(window_sign_up,text="Sign up",command=sign_up_webSite)
         btn_confirm_sign_up.place(x=180,y=120)
+    #播放背景音
+    def playBgMusic(self):
+
+        pygame.mixer.init()
+
+        if not pygame.mixer.music.get_busy():
+            playMusic = r"E:\\Pythonwork\\source\\music\\dyht.Ogg"
+            pygame.mixer.music.load(playMusic)
+            pygame.mixer.music.play(1)
+            print
+            'playing...', playMusic
+        else:
+            time.sleep(1)
+
 
 if __name__=="__main__":
     UILogin()
